91e5a85486142540b2aaae6bb2a2d822491798ba
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-15 11:42:19 -0300

    build(storage): configure Active Storage and project settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :documents, only: [:index, :new, :create]
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,5 +9,5 @@ Rails.application.routes.draw do
   #get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "posts#index"
+  root "documents#index"
 end
