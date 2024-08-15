2c494589ba95b2531ae8c09c59626c4f4666a040
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-15 11:42:35 -0300

    feat(documents): create Document model with migration

---
+++ app/models/document.rb
@@ -0,0 +1,21 @@
+class Document < ApplicationRecord
+  has_one_attached :file
+
+  before_save :set_title_from_file_name, if: -> { file.present? }
+
+  validate :correct_file_mime_type
+
+  private
+
+  def set_title_from_file_name
+    self.title = file.filename.base if file.attached?
+  end
+
+  def correct_file_mime_type
+    if file.attached? && !file.content_type.in?(%w(application/xml text/xml))
+      errors.add(:file, I18n.t('errors.invalid_file_type'))
+    elsif file.attached? == false
+      errors.add(:file, I18n.t('errors.file_required'))
+    end
+  end
+end
