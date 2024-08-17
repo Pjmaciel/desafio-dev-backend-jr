a4fe7ab926e6646e8782ddfcbad8edd817b03e00
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-17 20:54:03 -0300

    feat(upload): implement document upload with file type validation

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,6 @@
 class Document < ApplicationRecord
   has_one_attached :file
-  has_one :processed_document, dependent: :destroy
+  has_many :processed_documents, dependent: :destroy
 
   before_save :set_title_from_file_name, if: -> { file.present? }
 
@@ -13,7 +13,7 @@ class Document < ApplicationRecord
   end
 
   def correct_file_mime_type
-    if file.attached? && !file.content_type.in?(%w(application/xml text/xml))
+    if file.attached? && !file.content_type.in?(%w(application/xml text/xml application/zip))
       errors.add(:file, I18n.t('errors.invalid_file_type'))
     elsif file.attached? == false
       errors.add(:file, I18n.t('errors.file_required'))
