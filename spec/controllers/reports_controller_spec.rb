a8d54daf71bd5d7d79c83880580cf27746479aa7
Pjmaciel <pjmaciel_4@hotmail.com>
2024-08-18 00:52:03 -0300

    test(reports): tests for xls export

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -23,21 +23,32 @@ RSpec.describe ReportsController, type: :controller do
 
     it "assigns all products to @all_products" do
       get :show, params: { id: processed_document.id }
-      expect(assigns(:all_products)).to eq(assigns(:report_data)[:produtos])
+
+      expected_products = assigns(:report_data)[:produtos]
+
+      assigns(:all_products).each_with_index do |product, index|
+        expect(product).to include(
+                             nome: expected_products[index][:nome],
+                             ncm: expected_products[index][:ncm],
+                             cfop: expected_products[index][:cfop]
+                           )
+      end
     end
+
+
 
     context "when query is present" do
       it "assigns the filtered results to @filtered_results" do
         get :show, params: { id: processed_document.id, query: 'Produto A' }
-        filtered_products = assigns(:filtered_results).first[:produtos] # Acessa os produtos dentro de @filtered_results
-        expect(filtered_products).to include(hash_including(nome: 'Produto A'))
+        filtered_products = assigns(:filtered_results).first[:produtos]
+        expect(filtered_products.map { |p| p.transform_keys(&:to_sym) }).to include(hash_including(nome: 'Produto A'))
       end
     end
 
     context "when filters are applied" do
       it "assigns the filtered products to @filtered_products" do
         get :show, params: { id: processed_document.id, nome: 'Produto A' }
-        filtered_products = assigns(:filtered_products) # @filtered_products já deve ser o array de produtos
+        filtered_products = assigns(:filtered_products).map { |p| p.transform_keys(&:to_sym) }
         expect(filtered_products).to include(hash_including(nome: 'Produto A'))
       end
     end
--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -19,5 +19,21 @@ RSpec.describe "Reports", type: :request do
       expect(response.body).to include("CNPJ do Emitente")
       expect(response.body).to include("Total de Produtos")
     end
+
+    it "displays the 'Exportar para Excel' button" do
+      get report_path(processed_document)
+
+      # Verifica se o botão "Exportar para Excel" está presente na página
+      expect(response.body).to include("Exportar para Excel")
+    end
+
+    it "exports the report to Excel when the button is clicked" do
+      # Simula o clique no botão para exportar o relatório
+      get export_to_excel_report_path(processed_document, format: :xlsx)
+
+      # Verifica se a resposta é bem-sucedida (código de status 200) e o tipo de conteúdo esperado
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to include "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    end
   end
 end
